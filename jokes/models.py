700235f990b3a26514e92a977ad14d7dff869c04
JanMilan <jan.milan@luxairgroup.lu>
2026-02-03 11:20:12 +0100

    adding jokes list view

--- jokes/models.py
+++ jokes/models.py
@@ -7,7 +7,10 @@ class Joke(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        db_table = 'jokes'
+        ordering = ['question', 'answer']
+
     def __str__(self):
         return self.question
-    
     
