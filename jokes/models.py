7eccfaf9d202a83eef2041716f5fc61d135c1a03
JanMilan <jan.milan@luxairgroup.lu>
2026-02-04 15:23:10 +0100

    adding slugs

--- jokes/models.py
+++ jokes/models.py
@@ -1,21 +1,30 @@
 from django.db import models
 from django.urls import reverse
-from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+from common.utils.text import unique_slug
+
+
 class Joke(models.Model):
-    """Joke: A single joke"""
     question = models.TextField(max_length=200)
     answer = models.TextField(max_length=100, blank=True)
+
+    slug = models.SlugField(
+        max_length=50,
+        unique=True,
+        null=False,
+        editable=False,
+    )
+
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
-    class Meta:
-        db_table = 'jokes'
-        ordering = ['question', 'answer']
-
     def get_absolute_url(self):
-        return reverse('jokes:detail', args=[str(self.pk)])
+        return reverse("jokes:detail", args=[self.slug])
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = unique_slug(str(self), self.__class__)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.question
