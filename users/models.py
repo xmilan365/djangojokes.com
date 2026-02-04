d31c7551088117c8bf06de083d6131d68c6c2926
JanMilan <jan.milan@luxairgroup.lu>
2026-02-04 13:06:40 +0100

    adding superuser

--- users/models.py
+++ users/models.py
@@ -1,8 +1,7 @@
 from django.db import models
-
-# Create your models here.
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+# Create your models here.
 class CustomUser(AbstractUser):
     pass
