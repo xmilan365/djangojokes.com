954db388ce4de4d7796454f5bdca268e9316c488
JanMilan <jan.milan@luxairgroup.lu>
2026-02-06 11:51:43 +0100

    adding tags

--- jokes/admin.py
+++ jokes/admin.py
@@ -1,10 +1,33 @@
 from django.contrib import admin
-from .models import Joke
+
+from .models import Category, Joke, Tag
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    model = Category
+    list_display = ['category', 'created', 'updated']
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj: # editing an existing object
+            return ('slug', 'created', 'updated')
+        return ()
+    
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    model = Tag
+    list_display = ['tag', 'created', 'updated']
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj: # editing an existing object
+            return ('slug', 'created', 'updated')
+        return ()
+
 @admin.register(Joke)
 class JokeAdmin(admin.ModelAdmin):
     model = Joke
     list_display = ['question', 'created', 'updated']
+
     def get_readonly_fields(self, request, obj=None):
-        if obj:  # editing an existing object
+        if obj: # editing an existing object
             return ('slug', 'created', 'updated')
         return ()
--- jokes/models.py
+++ jokes/models.py
@@ -3,28 +3,72 @@ from django.urls import reverse
 
 from common.utils.text import unique_slug
 
-
-class Joke(models.Model):
-    question = models.TextField(max_length=200, help_text="The first part of the joke")
-    answer = models.TextField(max_length=100, blank=True, help_text="The second part of the joke")
-
+class Category(models.Model):
+    category = models.CharField(max_length=50)
     slug = models.SlugField(
-        max_length=50,
-        unique=True,
-        null=False,
-        editable=False,
+        max_length=50, unique=True, null=False, editable=False
     )
-
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     def get_absolute_url(self):
-        return reverse("jokes:detail", args=[self.slug])
+        return reverse('jokes:category', args=[self.slug])
 
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = unique_slug(str(self), self.__class__)
+            value = str(self)
+            self.slug = unique_slug(value, type(self))
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.category
+    class Meta: 
+        verbose_name_plural = 'Categories'
+        ordering = ['category']
+
+class Tag(models.Model):
+    tag = models.CharField(max_length=50)
+    slug = models.SlugField(
+        max_length=50, unique=True, null=False, editable=False
+    )
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def get_absolute_url(self):
+        return reverse('jokes:tag', args=[self.slug])
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            value = str(self)
+            self.slug = unique_slug(value, type(self))
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.tag
+    class Meta: ordering = ['tag']
+    
+class Joke(models.Model):
+    question = models.TextField(max_length=200)
+    tags = models.ManyToManyField('Tag')
+    answer = models.TextField(max_length=100, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    slug = models.SlugField(
+        max_length=50, unique=True, null=False, editable=False
+    )
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def get_absolute_url(self):
+        return reverse('jokes:detail', args=[self.slug])
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            value = str(self)
+            self.slug = unique_slug(value, type(self))
+
         super().save(*args, **kwargs)
 
     def __str__(self):
         return self.question
+
+
