7420329531aefc444aae7e04c73a0538e3af1b00
JanMilan <jan.milan@luxairgroup.lu>
2026-03-02 10:30:19 +0100

    adding update succ message

--- jokes/views.py
+++ jokes/views.py
@@ -3,6 +3,10 @@ from django.views.generic import (DetailView, ListView, CreateView, UpdateView, 
 from .models import Joke
 from django.urls import reverse_lazy
 from .forms import JokeForm
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
+
 
 # Create your views here.
 class JokeListView(ListView):
@@ -11,13 +15,19 @@ class JokeListView(ListView):
 class JokeDetailView(DetailView):
     model = Joke
 
-class JokeCreateView(CreateView):
+class JokeCreateView(SuccessMessageMixin, LoginRequiredMixin, CreateView):
     model = Joke
     form_class = JokeForm
+    success_message = 'Joke created.'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
     
-class JokeUpdateView(UpdateView):
+class JokeUpdateView(SuccessMessageMixin, UpdateView):
     model = Joke
     form_class = JokeForm
+    success_message = 'Update Successful'
 
 class JokeDeleteView(DeleteView):
     model = Joke
