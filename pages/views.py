42dc82ca1eb027c4be49e4c17b25eabf69d16a53
JanMilan <jan.milan@luxairgroup.lu>
2026-03-02 10:13:18 +0100

    adding messages into about-us

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.contrib import messages
 
 # Create your views here.
+
+
 class HomePageView(TemplateView):
     template_name = 'pages/home.html'
 
+
 class AboutUsView(TemplateView):
     template_name = 'pages/about_us.html'
+
+    def get(self, request, *args, **kwargs):
+        messages.debug(request, 'Debug message.')
+        messages.info(request, 'Info message.')
+        messages.success(request, 'Success message.')
+        messages.warning(request, 'Warning message.')
+        messages.error(request, 'Error message.')
+        return super().get(request, args, kwargs)
