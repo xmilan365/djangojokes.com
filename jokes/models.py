1c1074dbf0768155d4566c18cb3ba08622c313f5
JanMilan <jan.milan@luxairgroup.lu>
2026-03-03 16:00:55 +0100

    ...fin

--- jokes/models.py
+++ jokes/models.py
@@ -1,10 +1,9 @@
 from django.conf import settings
 from django.db import models
-from django.db.models import Avg
+from django.db.models import Avg, Count, Sum
 from django.urls import reverse
 
 from common.utils.text import unique_slug
-
 
 
 class Joke(models.Model):
@@ -26,7 +25,7 @@ class Joke(models.Model):
     @property
     def num_votes(self):
         return self.jokevotes.count()
-    
+
     @property
     def num_likes(self):
         return self.jokevotes.filter(vote=1).count()
@@ -36,14 +35,14 @@ class Joke(models.Model):
         return self.jokevotes.filter(vote=-1).count()
 
     @property
-    def rating(self): 
-        if self.num_votes == 0: # No jokes, so rating is 0 
-            return 0 
-        
-        r = JokeVote.objects.filter(joke=self).aggregate(average=Avg('vote')) 
-        
-        # Return the rounded rating. 
-        
+    def rating(self):
+        if self.num_votes == 0:  # No jokes, so rating is 0
+            return 0
+
+        r = JokeVote.objects.filter(joke=self).aggregate(average=Avg('vote'))
+
+        # Return the rounded rating.
+
         return round(5 + (r['average'] * 5), 2)
 
     def get_absolute_url(self):
@@ -57,6 +56,26 @@ class Joke(models.Model):
 
     def __str__(self):
         return self.question
+
+    @property
+    def votes(self):
+        result = JokeVote.objects.filter(joke=self).aggregate(
+            num_votes=Count('vote'),
+            sum_votes=Sum('vote')
+        )
+
+        # If there aren't any votes yet, return a dictionary with values of 0.
+        if result['num_votes'] == 0:
+            return {'num_votes': 0, 'rating': 0, 'likes': 0, 'dislikes': 0}
+
+        # Otherwise, calculate the dict values using num_votes and sum_votes.
+        result['rating'] = round(
+            5 + ((result['sum_votes']/result['num_votes'])*5), 2
+        )
+        result['dislikes'] = int((result['num_votes'] - result['sum_votes'])/2)
+        result['likes'] = result['num_votes'] - result['dislikes']
+
+        return result
 
 
 class Category(models.Model):
